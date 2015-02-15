bec9cd754d986c1ed9bf09d1a0e50724bcf60378
Tomona Nanase <nanase@users.noreply.github.com>
2015-02-15 19:17:00 +0900

    Extension.csを追加

---
+++ src/Extension.cs
@@ -0,0 +1,50 @@
+﻿//
+// Extension.cs
+//
+// Author:
+//       Tomona Nanase <nanase@users.noreply.github.com>
+//
+// The MIT License (MIT)
+//
+// Copyright (c) 2015 Tomona Nanase
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+using System;
+using System.Linq;
+
+namespace Lury.Lexgen
+{
+    public static class Extension
+    {
+        /// <summary>
+        /// 文字列の配列を結合した文字列を生成します。
+        /// </summary>
+        /// <returns>文字列の配列が結合された文字列。</returns>
+        /// <param name="array">結合される文字列の配列。</param>
+        public static string ToArrayString(this string[] array)
+        {
+            if (array == null)
+                return "null";
+            else
+                return "new string []{ " + string.Join(", ", array.Select(s => "\"" + s + "\"")) + "}";
+        }
+    }
+}
+
